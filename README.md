928770f461b855829001aeaaf243ad3f75fb0a5b
Fraz2701 <98520804+Fraz2701@users.noreply.github.com>
2022-01-27 09:32:16 +0000

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Open this page at [https://fraz2701.github.io/stacker/](https://fraz2701.github.io/stacker/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/fraz2701/stacker** and import
+
+## Edit this project ![Build status badge](https://github.com/fraz2701/stacker/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/fraz2701/stacker** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/fraz2701/stacker/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,76 @@
+input.onButtonPressed(Button.A, function () {
+    if (Row < 4) {
+        if (Col != Stack) {
+            Playing = 0
+            basic.pause(1000)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+            basic.showIcon(IconNames.No)
+        } else if (Row == 0) {
+            Playing = 0
+            basic.pause(1000)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+            basic.showIcon(IconNames.Happy)
+        } else {
+            Row += -1
+        }
+    } else {
+        Stack = Col
+        Row += -1
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    Playing = 0
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    direction = 0
+    Stack = 0
+    Col = 0
+    Row = 4
+    Playing = 1
+})
+let direction = 0
+let Stack = 0
+let Col = 0
+let Playing = 0
+let Row = 0
+Row = 4
+Playing = 1
+basic.forever(function () {
+    if (Playing) {
+        led.unplot(Col, Row)
+        // Going left
+        // or
+        // Going right
+        //                 
+        if (direction) {
+            Col += -1
+            if (Col == 0) {
+                direction = 0
+            }
+        } else {
+            Col += 1
+            if (Col == 4) {
+                direction = 1
+            }
+        }
+        led.plot(Col, Row)
+        basic.pause(100)
+    }
+})
